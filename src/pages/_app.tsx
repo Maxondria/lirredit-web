71f171c52fa190896076479623dc3373ca577fac
Maxondria <maxtayebwa@gmail.com>
2020-11-22 00:26:54 +0300

    - update cache on login and register

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,95 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/react";
-
+import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
+import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
 import theme from "../theme";
+import React from "react";
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "../generated/graphql";
 
-function MyApp({ Component, pageProps }) {
+function betterUpdateQuery<MutationResult, QueryResult>(
+  cache: Cache,
+  queryInput: QueryInput,
+  mutationResult: any,
+  updateFn: (r: MutationResult, q: QueryResult) => QueryResult
+) {
+  return cache.updateQuery(
+    queryInput,
+    (data) => updateFn(mutationResult, data as any) as any
+  );
+}
+
+const client = createClient({
+  url: "http://localhost:4000/graphql",
+  fetchOptions: {
+    credentials: "include",
+  },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (loginMutationResult, _args, cache) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              {
+                query: MeDocument,
+              },
+              loginMutationResult,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+
+          register: (registerMutationResult, _args, cache) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              {
+                query: MeDocument,
+              },
+              registerMutationResult,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
+});
+
+function MyApp({ Component, pageProps }: any) {
   return (
-    <ThemeProvider theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <CSSReset />
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ThemeProvider>
+    <Provider value={client}>
+      <ThemeProvider theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <CSSReset />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
