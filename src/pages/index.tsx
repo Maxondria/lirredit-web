0f4af4ba003fafc5007d8ae63bb8f652f463a1e8
Maxondria <maxtayebwa@gmail.com>
2020-11-22 00:28:41 +0300

    - show navbar on all pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
-const Index = () => <div>Hello bro</div>;
+import React from "react";
+import NavBar from "../components/NavBar";
+
+const Index = () => (
+  <>
+    <NavBar />
+    <div>Hello bro</div>
+  </>
+);
 
 export default Index;
